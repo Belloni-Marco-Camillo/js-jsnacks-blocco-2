f93de444d6db9410ece78fa3fcd6c95d6c0159f3
Belloni-Marco-Camillo <bellonimarcocamillo@gmail.com>
2022-03-31 12:42:43 +0200

    first-es

---
+++ asset/js/main.js
@@ -0,0 +1,16 @@
+let i, count, numeroInput
+i = 0
+count = 0
+
+/* for (i = 0; i < 5; i++) {
+    numeroInput = parseInt(prompt(`inserisci numero`))
+    console.log(numeroInput);
+}
+ */
+
+
+while (count < 5) {
+    numeroInput = parseInt(prompt(`inserisci numero`))
+    console.log(numeroInput);
+    count++
+}
