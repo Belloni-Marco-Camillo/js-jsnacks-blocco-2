0338cfd232e325e461bfb35ca5ced171a09fbc3b
Belloni-Marco-Camillo <bellonimarcocamillo@gmail.com>
2022-03-31 12:51:26 +0200

    correction-es-one

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,16 +1,20 @@
-let i, count, numeroInput
+let i, count, numeroInput, somma
+somma = 0
 i = 0
 count = 0
 
-/* for (i = 0; i < 5; i++) {
+for (i = 0; i < 5; i++) {
     numeroInput = parseInt(prompt(`inserisci numero`))
-    console.log(numeroInput);
+    somma = somma + numeroInput
 }
- */
+console.log(somma);
 
 
+/* 
 while (count < 5) {
     numeroInput = parseInt(prompt(`inserisci numero`))
-    console.log(numeroInput);
+    somma = somma + numeroInput
+
     count++
 }
+console.log(somma); */
